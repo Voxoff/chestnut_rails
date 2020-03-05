5d69a9a5a58ff3f9ca999f9cd76fd2e0236199e4
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2020-03-05 21:32:13 +0000

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,6 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    if Rails.env.production?
-      raise # Force me to remember to change this
-    end
     origins '*'
 
     resource '*',
