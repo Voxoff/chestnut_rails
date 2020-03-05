d72edbd4fbb3b606f5d426833e2ce0c1364288e7
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2020-03-05 21:25:23 +0000

    url

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 TECHDEBT
 1. Resolve wrap_params in user_controller
-2.
-
-
-https://www.chestnut.com/tracker/Pledge
+2. Consider API and Websocket connections
+3. Consider X-Content-Type-Options: nosniff
+4. For size of requests: "Content-length" header
--- app/controllers/api/v1/trackers_controller.rb
+++ app/controllers/api/v1/trackers_controller.rb
@@ -2,14 +2,15 @@ class Api::V1::TrackersController < ApplicationController
   skip_before_action :authorized, only: [:show]
 
   def show
-    referrer_header = request.headers["REQUEST_URI"]
-    referrer_param = params[:id]
-    request.headers[:HTTP_REFERER]
-    binding.pry
-    Tracker.find_or_create_by(referrer: referrer_param).increment!(:count)
+    # referrer_header = request.headers["REQUEST_URI"]
+    # referrer_param = params[:id]
+    # request.headers[:HTTP_REFERER]
+    return unless params[:url]
+    Tracker.find_or_create_by(referrer: params[:url]).increment!(:count)
 
-    respond_to do |format|
-      format.js { render "lib/tracker.js", url: api_v1_tracker(referrer_param) }
-    end
+  end
+
+  def url_params
+    params.permit(:url)
   end
 end
